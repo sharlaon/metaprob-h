3c3b69cb875dc06d47ac07d80d8ec119c2222f07
Jay Pottharst <sharlaon@gmail.com>
2018-11-28 18:17:21 -0600

    Rewrote; first draft Tracing

--- metaprob.hs
+++ metaprob.hs
@@ -1,109 +1,97 @@
+-- Helpers
+enumValues :: (Enum a) => [a]
+enumValues = enumFrom (toEnum 0)
+test :: (Eq a) => a -> a -> b -> b -> b
+test x y u v = if x == y then u else v
 
-{-# LANGUAGE FunctionalDependencies #-}
+data Traced keys values = Trace
+  { val :: values
+  , tr :: [(keys,String)] }
+trace :: (Show values) => values -> [(keys,values)] -> Traced keys values
+trace v ts = Trace v $ map (\kx -> (fst kx,show $ snd kx)) ts
+instance (Eq keys, Eq values) => Eq (Traced keys values) where
+  xs == yt = val xs == val yt && tr xs == tr yt
+instance (Enum keys, Enum values) => Enum (Traced keys values) where
+  toEnum = undefined
+  fromEnum = undefined
+instance Functor (Traced keys) where
+  fmap f t = Trace (f $ val t) (tr t)
+tunit :: values -> (Traced keys values)
+tunit v = Trace v []
 
-newtype Distribution subsets = Distribution
-  { mu :: subsets -> Double }
+class DistrOps distrs where
+  -- Needed for runGen
+  dirac :: (Eq elts) => elts -> distrs elts
+  convolve :: (Enum elts) =>
+              (distrs elts) -> (elts -> distrs elts) -> distrs elts
+  -- Needed for runTracing
+  traceless :: (distrs elts) -> distrs (Traced keys elts)
 
-newtype Derivative elements = Derivative
-  { nu :: elements -> Double}
+data DDistrs distrs elts = DDistr
+  { sample :: distrs elts
+  , score :: elts -> Double }
 
-data DDistribution elements subsets = DDistribution
-  { sample :: Distribution subsets
-  , score :: Derivative elements }
+tracelessd :: (DistrOps distrs) =>
+  (DDistrs distrs elts) -> DDistrs distrs (Traced keys elts)
+tracelessd dd = DDistr (traceless $ sample dd) ((score dd) . val)
 
-data GenerativeFunction keys elements subsets =
-  Sample keys (DDistribution elements subsets) |
-  Ret elements |
-  Semicolon
-    (GenerativeFunction keys elements subsets)
-    (elements -> GenerativeFunction keys elements subsets)
+data GenFn keys distrs elts =
+  Sample keys (DDistrs distrs elts) |
+  Ret elts |
+  Semicolon (GenFn keys distrs elts) (elts -> GenFn keys distrs elts)
 
--- These are the structures required to define runGen.
--- Note: If Distribution satisfies
---   Distribution subsets === m elements
--- where m is a monad, then we get a measure space instance via
---   dirac = return
---   convolve = bind
-class MeasureSpace elements subsets | subsets -> elements where
-  dirac :: elements -> Distribution subsets
-  convolve ::
-    (Distribution subsets) ->
-    (elements -> Distribution subsets) ->
-    Distribution subsets
-
-runGen :: (MeasureSpace elements subsets) =>
-  (GenerativeFunction keys elements subsets) ->
-  Distribution subsets
+runGen :: (Eq elts, Enum elts, DistrOps distrs) =>
+          (GenFn keys distrs elts) -> distrs elts
 runGen (Sample key dist) = sample dist
-runGen (Ret e) = dirac e
+runGen (Ret elt) = dirac elt
 runGen (Semicolon p1 p2) = convolve (runGen p1) (runGen . p2)
 
+runTracing :: (Eq elts, Enum elts, Show elts, DistrOps distrs) =>
+  (GenFn keys distrs elts) -> GenFn keys distrs (Traced keys elts)
+runTracing (Sample key dist) = Semicolon
+  (Sample key (tracelessd dist))
+  (\xt -> Ret $ trace (val xt) [(key,val xt)])
+runTracing (Ret elt) = Ret $ tunit elt
+runTracing (Semicolon p1 p2) =
+  Semicolon
+    (runTracing p1)
+    (\xs -> Semicolon
+      (runTracing (p2 (val xs)))
+      (\yt -> Ret $ Trace (val yt) ((tr xs) ++ (tr yt)) ))
 
 -- Meta-example
--- If everything :: [a] is finite with distinct elements, then it
--- determines a uniform probability space.  The elements of this space
--- are the objects UE (everything,e) where e `elem` everything.  The
--- subsets of this space, all deemed measurable, correspond to objects
--- US (everything,s) where s is a subset of the elements of
--- everything, made into a list using the ordering implicit in
--- everything.
-data UniformElements a = UE { ue :: ([a],a) }
-data UniformSubsets a = US { us :: ([a],[a]) }
-instance (Eq a) =>
-         MeasureSpace (UniformElements a) (UniformSubsets a) where
-  dirac e = Distribution (\s ->
-    if (snd (ue e)) `elem` (snd (us s)) then 1.0 else 0.0)
-  convolve d1 d2 = Distribution (\u  ->
-    let (everything,subset) = us u in
-    sum $ map
-      (\y -> sum $ map
-        (\x ->
-           ((mu (d2 (UE (everything,x)))) (US (everything,[y])))
-           * ((mu d1) (US (everything,[x]))))
-        everything)
-      subset)
--- Helpers
-fromElements atElem = Distribution (\u ->
-  let (everything,subset) = us u in
-  sum $ map (\x -> atElem (UE (everything,x))) subset)
-toElements d e = let (everything,element) = ue e in
-  (mu d) $ US (everything,[element])
+data Distrs elts = Distr { mu :: elts -> Double }
+instance DistrOps Distrs where
+  dirac elt = Distr (\elt' -> test elt elt' 1.0 0.0)
+  convolve d1 d2 =
+    Distr (\elt' ->
+      sum $ map (\elt -> ((mu (d2 elt)) elt') * ((mu d1) elt)) enumValues)
+  traceless distr = Distr (\xt ->
+    if null (tr xt) then (mu distr) (val xt) else 0.0)
 
 -- Infra-example:
 -- All the unknown random state consists of a single coin toss outcome.
-data MySet = Tails | Heads deriving (Show, Ord, Eq)
+data MySet = Tails | Heads deriving (Show, Eq, Enum)
 mySet = [Tails, Heads]
+makeDDistr f = DDistr (Distr f) f
 
-initialKnowledge = Ret $ UE (mySet, Tails)
-initialKnowledge2Elems (UE (everything,Tails)) = 0.8
-initialKnowledge2Elems (UE (everything,Heads)) = 0.2
-initialKnowledge2Dist =
-  DDistribution
-    (fromElements initialKnowledge2Elems)
-    (Derivative initialKnowledge2Elems)
-initialKnowledge2 = Sample 0 $ initialKnowledge2Dist
+input = Ret Tails
+input2Elems Tails = 0.8
+input2Elems Heads = 0.2
+input2 = Sample 0 $ makeDDistr input2Elems
+drunkenNot key x = Sample key $ makeDDistr (\y -> test x y 0.1 0.9)
+computed = Semicolon (Semicolon input (drunkenNot 1)) (drunkenNot 2)
+computed2 = Semicolon (Semicolon input2 (drunkenNot 1)) (drunkenNot 2)
 
-drunkenNotBase :: (UniformElements MySet) -> (UniformSubsets MySet) -> Double
-drunkenNotBase _              (US (_,[]))            = 0.0
-drunkenNotBase (UE (_,Tails)) (US (_,[Heads]))       = 0.9
-drunkenNotBase (UE (_,Tails)) (US (_,[Tails]))       = 0.1
-drunkenNotBase (UE (_,Tails)) (US (_,[Tails,Heads])) = 1.0
-drunkenNotBase (UE (_,Heads)) (US (_,[Tails]))       = 0.9
-drunkenNotBase (UE (_,Heads)) (US (_,[Heads]))       = 0.1
-drunkenNotBase (UE (_,Heads)) (US (_,[Tails,Heads])) = 1.0
-drunkenNot ::
-  key -> (UniformElements MySet) ->
-  GenerativeFunction key (UniformElements MySet) (UniformSubsets MySet)
-drunkenNot key x =
-  let f = Distribution (\u -> drunkenNotBase x u) in
-    Sample key (DDistribution f (Derivative (toElements f)))
+debugGen :: (Distrs MySet) -> String
+debugGen d =
+  "Tails: " ++ (show $ (mu d) Tails) ++ ", Heads: " ++ (show $ (mu d) Heads)
 
-computedKnowledge =
-  Semicolon (Semicolon initialKnowledge (drunkenNot 1)) (drunkenNot 2)
-computedKnowledge2 =
-  Semicolon (Semicolon initialKnowledge2 (drunkenNot 1)) (drunkenNot 2)
+debugTrace :: (Distrs (Traced Integer MySet)) -> String
+debugTrace d =
+  "  Tails: " ++ (show $ mu d $ trace Tails []) ++
+  ", Heads: " ++ (show $ mu d $ trace Heads [])
 
-debug :: (Distribution (UniformSubsets MySet)) -> String
-debug d =
-  "Tails: " ++ (show $ (mu d) (US (mySet,[Tails]))) ++ ", Heads: " ++ (show $ (mu d) (US (mySet,[Heads])))
--- try: debug $ runGen computedKnowledge
+-- try:
+-- debugGen $ runGen computed
+-- debugTrace $ runGen $ runTrace $ computed
